6d2a044189418714f71bf86d0ee2d7a0a6518df8
Griesh Mehndiratta <37310454+GrieshMehndiratta@users.noreply.github.com>
2018-10-18 23:05:50 +0500

    It take memory when it goes to else condition

--- 5.Form a new number.c
+++ 5.Form a new number.c
@@ -1,6 +1,5 @@
 int evenDigits(int n)
 {
-  int ans,k;
   if(n<10 && n>-10)
   {
     if(n%2==0 && n!=1)
@@ -11,6 +10,7 @@ int evenDigits(int n)
   
   else
   {
+    int ans,k;
     k=n%10;
     ans = evenDigits(n/10);
     if(k%2==0 && k!=1)
